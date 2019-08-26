b652f2c64bf872d1c37c7ea4a0d0b66e7b20a319
Hugo De Moraes <demoraeshugo@gmail.com>
2019-08-26 18:59:04 -0400

    Updated JS scripts

--- JavaScript/custom.js
+++ JavaScript/custom.js
@@ -1,60 +1,4 @@
 
-/*$(document).ready(function () {
-    $(document).on("scroll", onScroll);
-    
-    //smoothscroll
-    $('a[href^="#"]').on('click', function (e) {
-        //e.preventDefault();
-        $(document).off("scroll");
-        
-        $('a').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-      
-        var target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top+2
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
-    });
-});
-
-function onScroll(event){
-    var scrollPos = $(document).scrollTop();
-    $('#navbarsExampleDefault1 a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('#navbarsExampleDefault ul li a').removeClass("active");
-            currLink.addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
-        }
-    });
-}*/
-
-
-/*function move() {
-    var elem = document.getElementById("myBar"); 
-    var width = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-      } else {
-        width++; 
-        elem.style.width = width + '%'; 
-      }
-    }
-  }*/
-
-
 var divs = $('div[id^="content-"]').hide(),
     i = 0;
 
@@ -89,13 +33,6 @@ $(document).ready(function() {
 
 $(window).scroll(function() {
     var scrollDistance = $(window).scrollTop();
-
-    // Show/hide menu on scroll
-    //if (scrollDistance >= 850) {
-    //		$('nav').fadeIn("fast");
-    //} else {
-    //		$('nav').fadeOut("fast");
-    //}
 
     // Assign active class to nav links while scolling
     $('.page-section').each(function(i) {
